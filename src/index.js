12c3f52bf5ba62f183afa18ecf56359a852e6f1e
unknown <edisonalzate11@gmail.com>
2021-04-18 14:22:07 -0500

    add files project ended

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HeroApp } from './HeroApp';
-
+import { HeroesApp } from './HeroesApp';
 
 
 ReactDOM.render(
-  <HeroApp />,
+  <HeroesApp />,
   document.getElementById('root')
 );
