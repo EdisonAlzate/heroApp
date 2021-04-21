a378990c5ed5403b4d01042b24ea88e8d941027b
unknown <edisonalzate11@gmail.com>
2021-04-21 14:04:14 -0500

    Docs added

--- src/LoginScreen/LoginScreen.js
+++ src/LoginScreen/LoginScreen.js
@@ -15,7 +15,7 @@ export const LoginScreen = ({history}) => {
     dispatch({
         type: types.login,
         payload: {
-            name: 'Fernando'
+            name: 'Edison'
         }
     });
 
